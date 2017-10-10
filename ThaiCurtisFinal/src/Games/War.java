f2881076eb0a0527fe5afc5146fe38498eb16d0f
Michael Curtis <michaelcurtis2421@gmail.com>
2017-10-10 09:09:36 -0500

    update

--- ThaiCurtisFinal/src/Games/War.java
+++ ThaiCurtisFinal/src/Games/War.java
@@ -32,22 +32,62 @@ public class War {
 			System.out.println("Player2's score is now: " + score);
 			
 			
-		}
-		
-		System.out.println("Would you like to play again? 1 for Yes, 2 for No");
+		}System.out.println("Would you like to play again? 1 for Yes, 2 for No");
 		choice = input.nextInt();
 		
-		while(choice == 1)
+		if(choice == 1)
 		{
+			playAgain();
+		}
+		else
+		{
+			System.exit(0);
+		}
+	}
+	
+		public static void playAgain()
+		{
+			Random rand = new Random();
+
+			int  player1 = rand.nextInt(50) + 1;
+			int  player2 = rand.nextInt(50) + 1;
+			int score = 0;
+			int choice;
+			Scanner input = new Scanner (System.in);
+			
 			System.out.println("Player1's number is: " + player1);
 			System.out.println("Player2's number is: " + player2);
-			break;
-		}
-		while(choice != 1)
+			
+			if(player1 > player2)
+			{
+				score ++;
+				System.out.println("Player1's score is now: " + score);
+				
+					
+			}
+			if(player2 > player1)
+			{
+				score ++;
+				System.out.println("Player2's score is now: " + score);
+		
+
+		
+		
+//		while(choice == 1)
+//		{
+//			System.out.println("Player1's number is: " + player1);
+//			System.out.println("Player2's number is: " + player2);
+//			break;
+//		}
+//		while(choice != 1)
 		{
-			System.out.println("You have exited the program");
-			break;
+//			System.out.println("You have exited the program");
+//			break;
 		}
+}
+		}
+}
+
 		
 //		else
 //		{
@@ -71,6 +111,6 @@ public class War {
 //		
 //		
 
-	}
+	
 
-}
+
