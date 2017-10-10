c218bc8d38b984571eb85128ba9944fccb8751c4
Michael Curtis <michaelcurtis2421@gmail.com>
2017-10-10 08:38:20 -0500

    update

--- ThaiCurtisFinal/src/Games/War.java
+++ ThaiCurtisFinal/src/Games/War.java
@@ -1,5 +1,6 @@
 package Games;
 import java.util.Random;
+import java.util.Scanner;
 public class War {
 
 	public static void main(String[] args) 
@@ -10,25 +11,54 @@ public class War {
 		int  player1 = rand.nextInt(50) + 1;
 		int  player2 = rand.nextInt(50) + 1;
 		int score = 0;
+		int choice;
+		Scanner input = new Scanner (System.in);
+		
 		
 		
 		System.out.println("Player1's number is: " + player1);
 		System.out.println("Player2's number is: " + player2);
 		
-		while(player1 > player2)
+		if(player1 > player2)
 		{
-			
+			score ++;
 			System.out.println("Player1's score is now: " + score);
-			score++;
-				break;
+			
+				
 		}
-		while(player2 > player1)
+		if(player2 > player1)
 		{
+			score ++;
 			System.out.println("Player2's score is now: " + score);
-			score++;
-				break;
+			
+			
 		}
 		
+		System.out.println("Would you like to play again? 1 for Yes, 2 for No");
+		choice = input.nextInt();
+		
+		while(choice == 1)
+		{
+			System.out.println("Player1's number is: " + player1);
+			System.out.println("Player2's number is: " + player2);
+			break;
+		}
+		if(player1 > player2)
+		{
+			score ++;
+			System.out.println("Player1's score is now: " + score);
+			
+				
+		}
+		if(player2 > player1)
+		{
+			score ++;
+			System.out.println("Player2's score is now: " + score);
+//		else
+//		{
+//			System.out.println("You answered no");
+//			System.exit(0);
+//		}
 		
 		
 //	}
